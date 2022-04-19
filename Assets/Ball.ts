f1afc9c06bc9cdf31b4d962b08ca43a89f338f57
crusianz <60174331+crusianz@users.noreply.github.com>
2022-04-20 01:53:44 +0900

    공 물리 추가
    - rigidbody를 이용하여 공이 반사되는 것을 물리엔진에 맡기기로 하였다.
    - C#이 아닌 typescript라서 문법이 살짝 적응이 안된다.

--- Assets/Ball.ts
+++ Assets/Ball.ts
@@ -1,18 +1,30 @@
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
 import { UnityEvent } from "UnityEngine.Events";
-
+import { Transform, Vector3, Time, Rigidbody } from 'UnityEngine';
 
 
 export default class Ball extends ZepetoScriptBehaviour {
     private mEvent: UnityEvent;
-    public static Stage: int;
+    public static stage: int;
+    private speed: float;
+    private angle: Vector3;
+    private moving: bool;
+    public rb: Rigidbody;
+
 
     Start() {
         this.mEvent = new UnityEvent();
-        this.mEvent.AddListener(() => this.Ping());
+        this.mEvent.AddListener(() => this.Shot());
     }
 
-    Ping() {
+    Update() {
+        if (this.moving)
+        {
+            this.rb.AddForce
+        }
+    }
+
+    Shot() {
 
 
      }
